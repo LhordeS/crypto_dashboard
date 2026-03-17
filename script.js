b042cb648b0dd3e9919cfea76f678d4867d3e2f6
LhordeS <60381121+LhordeS@users.noreply.github.com>
2026-03-17 20:20:48 +0900

    Write script to display top 5 cryptocurrencies from fetched coinmarketcap.com api data in a list that includes a coin image, coin name, current price, 24h price change. Also made list item display green if 24h price change was positive, and red if negative

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 "use strict";
+
+// const list = document.getElementById("list");
+
+// fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//     data.slice(0, 5).forEach((coin) => {
+//       const li = document.createElement("li");
+
+//       if (coin.price_change_percentage_24h > 0) {
+//         li.style.color = "green";
+//       } else if (coin.price_change_percentage_24h < 0) {
+//         li.style.color = "red";
+//       }
+
+//       const img = document.createElement("img");
+//       img.src = coin.image;
+//       img.width = 20;
+
+//       li.appendChild(img);
+//       li.appendChild(
+//         document.createTextNode(
+//           `  ${coin.name}: ${coin.current_price} (${coin.price_change_percentage_24h.toFixed(2)}%)`,
+//         ),
+//       );
+
+//       list.appendChild(li);
+//     });
+//   });
+
+const list = document.getElementById("list");
+
+fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+  .then((res) => res.json())
+  .then((data) => {
+    data.slice(0, 10).forEach((coin) => {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+
+      img.src = coin.image;
+      img.width = 20;
+      img.style.marginRight = "10px";
+
+      li.appendChild(img);
+      li.appendChild(document.createTextNode(`${coin.name}`));
+
+      list.appendChild(li);
+    });
+  });
